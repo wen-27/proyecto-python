585feba94aec93ad6b557ce2c675aa9b1911312b
wen-27 <192928145+wen-27@users.noreply.github.com>
2025-07-28 15:41:59 -0500

    feat: :tada: menus

---
+++ menus.py
@@ -0,0 +1,93 @@
+menu_principal = """
+===========================================
+        Administrador de Colección
+===========================================
+1. Añadir un Nuevo Elemento
+2. Ver Todos los Elementos
+3. Buscar un Elemento
+4. Editar un Elemento
+5. Eliminar un Elemento
+6. Ver Elementos por Categoría
+7. Guardar y Cargar Colección
+8. Salir
+===========================================
+Selecciona una opción (1-8): 
+"""
+
+nuevo_elemento = """
+===========================================
+        Añadir un Nuevo Elemento
+===========================================
+¿Qué tipo de elemento deseas añadir?
+1. Libro
+2. Película
+3. Música
+4. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-4):
+"""
+menu_ver_elementos = """
+===========================================
+        Ver Todos los Elementos
+===========================================
+¿Qué categoría deseas ver?
+1. Ver Todos los Libros
+2. Ver Todas las Películas
+3. Ver Toda la Música
+4. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-4):
+
+"""
+
+menu_buscar = """
+===========================================
+        Buscar un Elemento
+===========================================
+¿Cómo deseas buscar?
+1. Buscar por Título
+2. Buscar por Autor/Director/Artista
+3. Buscar por Género
+4. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-4):
+"""
+
+menu_editar = """
+===========================================
+        Editar un Elemento
+===========================================
+¿Qué tipo de cambio deseas realizar?
+1. Editar Título
+2. Editar Autor/Director/Artista
+3. Editar Género
+4. Editar Valoración
+5. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-5):
+"""
+
+ver_elementos_categoria = """
+===========================================
+        Ver Elementos por Categoría
+===========================================
+¿Qué categoría deseas ver?
+1. Ver Libros
+2. Ver Películas
+3. Ver Música
+4. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-4):
+"""
+
+guardar_cargar = """
+===========================================
+        Guardar y Cargar Colección
+===========================================
+¿Qué deseas hacer?
+1. Guardar la Colección Actual
+2. Cargar una Colección Guardada
+3. Regresar al Menú Principal
+===========================================
+Selecciona una opción (1-3):
+"""
