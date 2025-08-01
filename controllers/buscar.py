6a7ae3987ebedafb5e0e60e41df16f1f3cec08c1
corredor29 <corredorsilvafelipe8@gmail.com>
2025-07-31 21:13:08 -0500

    fix: :sparkles: Se arreglo la busqueda

--- controllers/buscar.py
+++ controllers/buscar.py
@@ -5,30 +5,32 @@ from utils.helpers import leer_json
 
 
 def buscar(termino, tipo):
-    resultados = []
+    bander = True
     try:
         # Libros
         libros = leer_json(archivo_libros)
         for libro in libros:
             if libro[tipo] == termino:
-                for i in libros:
-                    print(i)
+                bander = False
+                print("="*20, "Libro encontrado", "="*20)
+                print(libro)
 
         # Música
         musicas = leer_json(archivo_musica)
         for musica in musicas:
             if musica[tipo] == termino:
-                for i in musicas:
-                    print(i)
+                bander = False
+                print("="*20, "Música encontrado", "="*20)
+                print(musica)
 
         # Películas
         peliculas = leer_json(archivo_peliculas)
         for pelicula in peliculas:
             if pelicula[tipo] == termino:
-                for i in peliculas:
-                    print(i)
-
+                bander = False
+                print("="*20, "Película encontrado", "="*20)
+                print(pelicula)
+        if bander:
+            print("No se encontraron resultados.")
     except Exception as e:
         print(f"\nError en la edición: {str(e)}")
-    
-    return resultados
